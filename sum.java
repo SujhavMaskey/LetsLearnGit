71ce38fc2f8a42f845b83b4c4a65894ae0b9fbd1
Sujhav Maskey <sujhavmaskeyy@gmail.com>
2023-05-24 21:47:43 +0545

    added sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args){
         int a=2;
         int b=2;
-        system.out.println(a+b);
+        system.out.println("the sum is"+ (a+b));
     }
 }
